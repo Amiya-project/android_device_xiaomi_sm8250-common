160249e93b2cdccfdff66ae72649135f2ea37579
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:25:13 +0000

    sm8250-common: parts: Adapt popupcamera parts to S style
    Change-Id: Id69636b5356b7a56cb1b6ba74271905f6316e651
    (cherry picked from commit d8fc0c8d6238037ad2497616932f03afbe6b5605)

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
@@ -18,9 +18,11 @@
 package org.lineageos.settings.popupcamera;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
-public class PopupCameraSettingsActivity extends PreferenceActivity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class PopupCameraSettingsActivity extends CollapsingToolbarBaseActivity {
 
     private static final String TAG_POPUPCAMERA = "popupcamera";
 
@@ -30,7 +32,7 @@ public class PopupCameraSettingsActivity extends PreferenceActivity {
 
         getFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, new PopupCameraSettingsFragment(),
+                .replace(R.id.content, new PopupCameraSettingsFragment(),
                         TAG_POPUPCAMERA)
                 .commit();
     }
