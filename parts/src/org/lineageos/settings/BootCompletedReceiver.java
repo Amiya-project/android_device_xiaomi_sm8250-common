a9a24409bf60723caec5b8f008e398df0d3219e7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-06 10:52:43 +0530

    sm8250-common: Introduce displayfeature color service and use MIUI color modes
    This service relays the current AOSP color mode to the Xiaomi
    displayfeature HAL, matching stock behaviour. This is necessary to
    avoid colors being reset after turning the screen off, because
    displayfeature sets color mode everytime the screen is turned on
    defaulting to Vivid mode. Having this fixed it finally allows us to use Miui color modes in AOSP.
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-Authored-By: lolipuru <banstellazz@gmail.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    Signed-off-by: Shikhar <Krtonia@gmail.com>
    Change-Id: Icb463068163045cee43278f57d0e12283a3cc88c

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -18,11 +18,13 @@
 package org.lineageos.settings;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.display.ColorService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -33,5 +35,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
+        ColorService.startService(context);
     }
 }
