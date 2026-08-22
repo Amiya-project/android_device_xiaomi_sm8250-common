fe5e364c4b892dca0a7f8253ef01d2d0857abbeb
Tkiliay <sackclouths@gmail.com>
2026-08-22 16:35:33 +0800

    fixup! sm8250-common: Remove vendor/lineage device framework matrix inclusion
    Change-Id: I3d9896309ee3a9ddd12e4950130c1d90c44cff3c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -207,7 +207,7 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 endif
 
 # VINTF
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
